b74c3d1c3f7ee3ac562471e346280ec9af2915b3
Sergei <sergeyevrfr@gmail.com>
2024-04-25 13:06:55 +0400

    Created new test

--- src/test/java/SoftAssertionsPageTest.java
+++ src/test/java/SoftAssertionsPageTest.java
@@ -2,22 +2,49 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-public class SoftAssertionsPageTest {
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SoftAssertionsPageTest
+{
 
     @BeforeAll
     static void beforeAll()
     {
 
         Configuration.browserSize = "1920x1080";
-        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.baseUrl = "https://github.com";
         Configuration.pageLoadStrategy = "eager";
 
     }
 
     @Test
-    void FindJUnit5CodeTest() {
+    void FindJUnit5CodeTest()
+    {
 
+        open("/selenide/selenide");
 
+        $("#wiki-tab").click();
+        $("#wiki-pages-filter").click();
+        $("#wiki-pages-filter").type("SoftAssertions").pressTab();
+        $(byText("SoftAssertions")).shouldBe(visible);
+        $(byText("SoftAssertions")).click();
+        $$("div.markdown-heading").findBy(text("JUnit5")).sibling(0).shouldHave(text("""
+                @ExtendWith({SoftAssertsExtension.class})
+                 class Tests {
+                   @Test
+                   void test() {
+                     Configuration.assertionMode = SOFT;
+                     open("page.html");
+                 
+                     $("#first").should(visible).click();
+                     $("#second").should(visible).click();
+                   }
+                 }"""));
+
+        closeWebDriver();
 
     }
 }
